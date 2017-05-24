cf20e16341821ecc3b8c8901ce37f9644f3df7d3
Robert Passmore <robertmpassmore@gmail.com>
2017-05-24 21:55:57 +0200

    Clean - refactor InputParser to be more concise

--- MarsRover/InputParser.py
+++ MarsRover/InputParser.py
@@ -1,17 +1,18 @@
 def parse_input(file_name):
     try:
         file = open(file_name, "r")
+        lines_of_input = [line.strip('\n') for line in file.readlines()]
 
-        max_x, max_y = [int(coordinate) for coordinate in file.readline().strip('\n').split(" ")]
+        max_x, max_y = [int(coordinate) for coordinate in lines_of_input[0].split(" ")]
         grid_size = (max_x, max_y)
 
-        second_line = file.readline()
-        start_x, start_y = [int(coordinate) for coordinate in second_line.strip('\n').split(" ")[:-1]]
+        start_x, start_y = [int(coordinate) for coordinate in lines_of_input[1].split(" ")[:-1]]
         starting_coordinate = (start_x, start_y)
-        starting_bearing = second_line.strip('\n').split(" ")[-1]
+
+        starting_bearing = lines_of_input[1].strip('\n').split(" ")[-1]
         starting_position = {"coordinate": starting_coordinate, "bearing": starting_bearing}
 
-        command_sequence = [command for command in file.readline().strip('\n')]
+        command_sequence = [command for command in lines_of_input[2]]
     except IOError:
         print("IOError")
     finally:
