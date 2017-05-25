e07d2383f10a885610ec558a013ae02ec38655ff
Robert Passmore <robertmpassmore@gmail.com>
2017-05-25 05:34:27 +0200

    Feat - control the rover through the RoverController

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from MarsRover.InputParser import parse_input
 from MarsRover.Rover import Rover
 from MarsRover.Zone import Zone
+from test.RoverControllerTest import RoverController
 
 if __name__ == '__main__':
     # TODO: handle file location in OS generic way
     instruction_file = "../example-input-file.txt"
     grid_size, starting_position, command_sequence = parse_input(instruction_file)
+
     rover_zone = Zone(grid_size)
-
     rover = Rover(starting_position["coordinate"], starting_position["bearing"], rover_zone)
-    rover.save_command_sequence(command_sequence)
-    rover.execute_command_sequence()
-    rover.report_current_position()
 
-
+    rover_controller = RoverController(rover)
+    rover_controller.transmit_command_sequence_to_rover(command_sequence)
+    rover_controller.instruct_rover_to_execute_command_sequence()
