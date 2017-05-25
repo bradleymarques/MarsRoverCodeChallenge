90670a61586f761cebee08d47809b157d25a1f56
Robert Passmore <robertmpassmore@gmail.com>
2017-05-25 07:45:04 +0200

    Clean - refactor

--- test/InputParserTest.py
+++ test/InputParserTest.py
@@ -4,17 +4,18 @@ from MarsRover import InputParser
 
 
 class TestInputParser(unittest.TestCase):
+    def setUp(self):
+        file_name = "example-input-file.txt"
+        self.grid_size, self.starting_position, self.command_sequence = InputParser.parse_input(file_name)
+
     def test_input_parser_finds_grid_size_in_file(self):
-        grid_size, starting_position, command_sequence = InputParser.parse_input("example-input-file.txt")
-        self.assertEqual(grid_size, (8, 8))
+        self.assertEqual(self.grid_size, (8, 8))
 
     def test_input_parser_finds_starting_position_in_file(self):
-        grid_size, starting_position, command_sequence = InputParser.parse_input("example-input-file.txt")
-        self.assertEqual(starting_position, {"coordinate": (1, 2), "bearing": "E"})
+        self.assertEqual(self.starting_position, {"coordinate": (1, 2), "bearing": "E"})
 
     def test_input_parser_finds_command_sequence_in_file(self):
-        grid_size, starting_position, command_sequence = InputParser.parse_input("example-input-file.txt")
-        self.assertEqual(command_sequence, [x for x in "MMLMRMMRRMML"])
+        self.assertEqual(self.command_sequence, [x for x in "MMLMRMMRRMML"])
 
 
 if __name__ == "__main__":
